da330debdc38aaa0da3d8699e51a64df6c7d2528
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-03-04 17:51:00 -0800

    Function concurrent writer graphs.

--- graphsapp/app.js
+++ graphsapp/app.js
@@ -1,8 +1,20 @@
 var couchapp = require('../../node.couch.js/couchapp/lib/couchapp'),
     path = require('path');
 
-ddoc = {_id:'app'}
+ddoc = {_id:'app',templates:{},modules:{},shows:{}}
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'))
+couchapp.loadFiles(ddoc.templates, path.join(__dirname, 'templates'))
+couchapp.loadModules(ddoc.modules, path.join(__dirname, 'modules'))
+
+ddoc.shows.writeTest = function (doc, req) {
+  var mustache = require('modules/mustache').Mustache;
+  var ddocPath = '/' + req.path.slice(0, 3).join('/');
+  var body = [mustache.to_html(this.templates['head.mustache'], {path:ddocPath}), 
+              mustache.to_html(this.templates['writeTest.mustache'], {results:JSON.stringify(doc.results),
+                                                                      clients:doc.clients}),
+              this.templates['foot.mustache']];
+  return body.join('\n')
+}
 
 exports.app = ddoc
--- graphsapp/sync.js
+++ graphsapp/sync.js
@@ -1,5 +1,4 @@
 var couchapp = require('../../node.couch.js/couchapp/lib/couchapp'),
     app = require('./app');
-    
 
 couchapp.sync(app.app, 'http://localhost:5984/graphs');
