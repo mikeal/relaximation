3b24c6d72f532abfa00cc127d3e4b5f4ac67f1e1
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-03-06 18:41:19 -0800

    Big improvements to the graph app.

--- graphsapp/app.js
+++ graphsapp/app.js
@@ -16,5 +16,37 @@ ddoc.shows.writeTest = function (doc, req) {
               this.templates['foot.mustache']];
   return body.join('\n')
 }
+ddoc.shows.writeReadTest = function (doc, req) {
+  var mustache = require('modules/mustache').Mustache;
+  var ddocPath = '/' + req.path.slice(0, 3).join('/');
+  var body = [mustache.to_html(this.templates['head.mustache'], {path:ddocPath}), 
+              mustache.to_html(this.templates['writeReadTest.mustache'], {results:JSON.stringify(doc.results),
+                                                                          rclients:doc.rclients, 
+                                                                          wclients:doc.wclients,
+                                                                          clients:doc.clients}),
+              this.templates['foot.mustache']];
+  return body.join('\n')
+}
+ddoc.shows.compareWriteTest = function (doc, req) {
+  var mustache = require('modules/mustache').Mustache;
+  var ddocPath = '/' + req.path.slice(0, 3).join('/');
+  var body = [mustache.to_html(this.templates['head.mustache'], {path:ddocPath}), 
+              mustache.to_html(this.templates['compareWriteTest.mustache'], {results:JSON.stringify(doc.results),
+                                                                             recurrence:doc.recurrence,
+                                                                             clients:doc.clients}),
+              this.templates['foot.mustache']];
+  return body.join('\n')
+}
+ddoc.shows.compareWriteReadTest = function (doc, req) {
+  var mustache = require('modules/mustache').Mustache;
+  var ddocPath = '/' + req.path.slice(0, 3).join('/');
+  var body = [mustache.to_html(this.templates['head.mustache'], {path:ddocPath}), 
+              mustache.to_html(this.templates['compareWriteReadTest.mustache'], {results:JSON.stringify(doc.results),
+                                                                             recurrence:doc.recurrence,
+                                                                             wclients:doc.wclients,
+                                                                             rclients:doc.rclients}),
+              this.templates['foot.mustache']];
+  return body.join('\n')
+}
 
 exports.app = ddoc
