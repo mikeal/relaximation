7c96f2a89b65734978ed9eb3d6873520eceee9b2
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-03-15 16:15:08 -0700

    Defaults are now show.

--- tests/compare_write_and_read.js
+++ tests/compare_write_and_read.js
@@ -9,17 +9,22 @@ var client = require("../common/client");
 var optionparser = require("../common/optionparser");
 var events = require('events');
 var opts = new optionparser.OptionParser();
-opts.addOption('-w', '--wclients', "number", "write_clients", 50, "Number of concurrent write clients per process.");
-opts.addOption('-r', '--rclients', "number", "read_clients", 200, "Number of concurrent read clients per process.");
-opts.addOption('-u', '--url1', "string", "url1", "http://localhost:5984", "CouchDB url to run tests against.");
-opts.addOption('-v', '--url2', "string", "url2", "http://localhost:5985", "CouchDB url to run tests against.");
-opts.addOption('-1', '--name1', "string", "name1", null, "Name of first comparative.");
-opts.addOption('-2', '--name2', "string", "name2", null, "Name of first comparative.");
-opts.addOption('-d', '--doc', "string", "doc", "small", "small or large doc.");
-opts.addOption('-t', '--duration', "number", "duration", 60, "Duration of the run in seconds.")
-opts.addOption('-i', '--poll', "number", "poll", 1, "Polling interval in seconds.")
-opts.addOption('-p', '--graph', "string", "graph", "http://couchdb.couchdb.org/graphs", "CouchDB to persist results in.")
-opts.addOption('-r', '--recurrence', "number", "recurrence", 10, "How many times to run the tests.")
+opts.addOption('-w', '--wclients', "number", "write_clients", 50, 
+               "Number of concurrent write clients per process. Default is 50.");
+opts.addOption('-r', '--rclients', "number", "read_clients", 200, 
+               "Number of concurrent read clients per process. Default is 200.");
+opts.addOption('-u', '--url1', "string", "url1", "http://localhost:5984", 
+               "CouchDB url to run tests against. Default is http://localhost:5984");
+opts.addOption('-v', '--url2', "string", "url2", "http://localhost:5985", 
+                "CouchDB url to run tests against. Default is http://localhost:5985");
+opts.addOption('-1', '--name1', "string", "name1", null, "Name of first comparative. Required.");
+opts.addOption('-2', '--name2', "string", "name2", null, "Name of first comparative. Required.");
+opts.addOption('-d', '--doc', "string", "doc", "small", "small or large doc. Default is small.");
+opts.addOption('-t', '--duration', "number", "duration", 60, "Duration of the run in seconds. Default is 60.")
+opts.addOption('-i', '--poll', "number", "poll", 1, "Polling interval in seconds. Default is 1.")
+opts.addOption('-p', '--graph', "string", "graph", "http://couchdb.couchdb.org/graphs", 
+               "CouchDB to persist results in. Default is http://couchdb.couchdb.org/graphs")
+opts.addOption('-r', '--recurrence', "number", "recurrence", 10, "How many times to run the tests. Deafult is 10.")
 
 var port = 8000;
 var ports = [];
