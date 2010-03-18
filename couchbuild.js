b8c571cd301baccfffc02ded0ccdb04950215691
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-03-17 17:09:53 -0700

    Making the build scripts rock again.

--- couchbuild.js
+++ couchbuild.js
@@ -1,127 +1,157 @@
 var sys = require('sys'),
-    os = require('./common/os'),
     path = require('path'),
+    fs = require('fs'),
     assert = require('assert'),
     events = require('events'),
     uuid = require('./common/uuid');
 
-var debugging = false;
-    
-var Builder = function (workingDir) {
-  if (!workingDir) { workingDir = path.join(process.cwd(), 'builddir-'+uuid.uuid2().replace('-',''))}
-  this.workingDir = workingDir;
-  if (!os.isdir(workingDir)) {
-    os.mkdir(workingDir);
+function queue (actions, callback) {
+  if (actions.length === 0) {
+    callback();
+    return;
   }
-}
-Builder.prototype.getGitSource = function (url, ref) {
-  var p = new events.Promise();
-  var dir = path.join(this.workingDir, 'git');
-  if (!ref) { ref = 'HEAD'}
-  p.name = 'git'; p.dir = dir; p.url = url; p.ref = ref;
-  setTimeout(function () {
-    assert.equal(0,  os.subprocessCall('git', ['clone', url, dir], undefined, undefined, debugging)[0]);
-    assert.equal(0,  os.subprocessCall('git', ['checkout', ref], undefined, dir, debugging)[0]);
-    p.emitSuccess(dir);
-  }, 0);
-  return p;
-}
-Builder.prototype.getTrunkSource = function() {
-  var p = new events.Promise
-  var dir = path.join(this.workingDir, 'svnTrunk');
-  var url = 'http://svn.apache.org/repos/asf/couchdb/trunk';
-  p.name = 'trunk'; p.dir = dir; p.url = url;
-  setTimeout(function () {
-    // assert.equal(0,  os.subprocessCall('svn', ['co', url, dir], undefined, undefined, debugging)[0]);
-    assert.equal(0, os.subprocessCall('cp', ['-R', '/Users/mikeal/tmp/relaxtests/svn/trunk', dir])[0])
-    p.emitSuccess(dir);
-  }, 0);
-  return p;
-}
-Builder.prototype.getReleaseSource = function(version) {
-  var url = 'http://svn.apache.org/repos/asf/couchdb/tags/'+version;
-  var p = new events.Promise
-  var dir = path.join(this.workingDir, 'release');
-  p.name = 'release'; p.dir = dir; p.url = url;
-  setTimeout(function () {
-    // assert.equal(0,  os.subprocessCall('svn', ['co', url, dir], undefined, undefined, debugging)[0]);
-    assert.equal(0, os.subprocessCall('cp', ['-R', '/Users/mikeal/tmp/relaxtests/svn/tags/0.10.1', dir])[0])
-    p.emitSuccess(path.join(dir));
-  }, 0);
-  return p;
-}
-Builder.prototype.build = function (dir) {
-  var commands = this.buildCommands[process.platform];
-  var p = new events.Promise();
-  setTimeout(function () {
-    for(i = 0; i < commands.length; i += 1) {
-      var cmd = commands[i];
-      assert.equal(0,  os.subprocessCall(cmd[0], cmd[1], undefined, dir, debugging)[0], cmd);
-    }
-    p.emitSuccess(dir);
-  }, 0)
-  return p;
-}
-Builder.prototype.configure = function (dir, port) {
-  var p = new events.Promise();
-  var config = path.join(dir, 'etc', 'couchdb', 'local_dev.ini');
-  setTimeout(function() {
-    os.write(config, os.read(config).replace(';port = 5984', 'port = '+port)
-                                    .replace(';bind_address = 127.0.0.1', 'bind_address = 0.0.0.0'));
-    p.emitSuccess(port);
-  }, 0);
-  return p;
+  var a = actions.shift();
+  if (typeof a === "function") {
+    var func = a;
+    var args = [];
+  } else if (typeof a === "object") {
+    var func = a.shift();
+    var args = a;
+  } else {
+    sys.puts("Invalid type in queue "+typeof(a))
+  }
+  args.push(function (error) {
+    if (error) {callback(error)}
+    else {queue(actions, callback)}
+  })
+  func.apply(func, args);
 }
 
-Builder.prototype.buildCommands = {
-  darwin:[['./bootstrap'],
+function parallel (actions, callback) {
+  var count = 0;
+  var expected = actions.length;
+  actions.forEach(function (a) {
+    a(function(error) {
+      if (error) {
+        sys.puts('Encountered error: '+error);
+        callback(error);
+        return;
+      } else {
+        count += 1;
+        if (count === expected) {
+          callback();
+        }
+      }
+    })
+  })
+}
+
+function subprocess (call, cmd, cwd, callback) {
+  sys.puts('command '+call+' '+cmd)
+  var buffer = '';
+  if (cwd) { var olddir = process.cwd(); process.chdir(cwd);}
+  var child = process.createChildProcess(call, cmd);
+  if (cwd) { process.chdir(olddir);}
+  child.addListener("output", function (data) {
+    if (data) { 
+      sys.print(data)
+      buffer += data; 
+    }
+  });
+  child.addListener("error", function(data) {
+    if (data) {
+      sys.print(data)
+      buffer += data;
+    }
+  });
+  child.addListener("exit", function(code) {
+    if (callback) {
+      callback(undefined, code, buffer);
+    }});
+}
+
+buildCommands = {
+  darwin:[['./bootstrap', []],
           ['./configure',['--with-js-include=/usr/local/spidermonkey/include', 
                           '--with-js-lib=/usr/local/spidermonkey/lib']],
-          ['make'],
+          ['make', []],
           ['make',['dev']],
           ]
 }
 
-
-
-var doBuilds = function (startPort) {
-  var currentPort = startPort;
+function builder (builds, workingDir) {
+  var buildqueues = [];
+  builds.forEach(function (info) {
+    var w = path.join(workingDir, uuid.uuid2().replace('-', ''));
+    var buildq = [
+      function (callback) {
+        subprocess('git', ['clone', info.giturl, w], workingDir, callback)
+      },
+      function (callback) {
+        subprocess('git', ['checkout', info.gitcheckout], w, callback)
+      },
+      function (callback) {
+        subprocess('./bootstrap', [], w, callback)
+      },
+      function (callback) {
+        subprocess('./configure', ['--with-js-include=/usr/local/spidermonkey/include', 
+                                   '--with-js-lib=/usr/local/spidermonkey/lib'], w, callback)
+      },
+      function (callback) {
+        subprocess('make', [], w, callback)
+      },
+      function (callback) {
+        subprocess('make', ['dev'], w, callback)
+      },
+      function (callback) {
+        if (info.port && info.port != 5984) {
+          var config = path.join(w, 'etc', 'couchdb', 'local_dev.ini')
+          fs.readFile(config, function (error, data) {
+            fs.writeFile(config, data.replace(';port = 5984', 'port = '+info.port), function (error) {
+              if (error) {callback(error)}
+              else {callback()}
+            })
+          })
+        } else {
+          callback();
+        }
+      },
+      function (callback) {
+        subprocess(path.join(w, 'utils', 'run'), [], undefined);
+        callback();
+      }
+      ]
+    buildqueues.push(buildq);
+  })
   
-  var b = new Builder();
-  var promises = [// b.getGitSource('git://github.com/mikeal/couchdb.git'), 
-                  b.getTrunkSource(),
-                  b.getReleaseSource('0.10.1'),
-                  ]                
-  
-  var results = [];
-          
-  var resultsPromise = new events.Promise();        
-                    
-  promises.forEach(function(p){
-    p.port = currentPort;
-    currentPort += 1;
-    p.addCallback( function(dir) { 
-      sys.puts('Bulding '+p.name)
-      b.build(dir).addCallback( function (dir) {
-        sys.puts('Configuring '+p.name)
-        var pr = b.configure(dir, p.port);
-        pr.addCallback(function (port) {
-          results.push({name:p.name,port:p.port,dir:p.dir,url:p.url,ref:p.ref});
-          if (results.length == promises.length) {
-            resultsPromise.emitSuccess(results);
-          }
-        })
-      })
+  var args = []
+  buildqueues.forEach(function (q) {
+    args.push(function (callback) {
+      queue(q, callback)
     })
   })
-  resultsPromise.wait();
-  results.forEach(function(result) {
-    result['subprocess'] = process.createChildProcess(path.join(result.dir, 'utils', 'run'));
-    result['subprocess'].addListener('error', function(data) {if(data){sys.print(data)}});
+  
+  parallel(args, function (error) {
+    sys.puts('finished')
   })
-  return results
+  
 }
 
-sys.puts(JSON.stringify(doBuilds(1111)))
+exports.builder = builder;
+exports.subprocess = subprocess;
+exports.queue = queue;
+exports.parallel = parallel;
 
-
+// builder([
+//   {name:"file_work", 
+//    giturl:"git://github.com/Damienkatz/couchdb.git",
+//    gitcheckout:"57904bb1030260b91f18668b3ffbadd7ed954e0d",
+//    arch:"darwin",
+//   },
+//   {name:"trunk",
+//    giturl:"git://github.com/apache/couchdb.git",
+//    gitcheckout:"HEAD",
+//    arch:"darwin", 
+//    port:5985,
+//   },
+//   ], path.join(__dirname, "builds"))
